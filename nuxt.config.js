3733cf43242a4b1aaf19b93f9330c2911cee7fc9
vano20 <vano_202228@yahoo.com>
2022-05-23 01:21:00 +0700

    add variable style

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/index.scss'],
+  css: ['@/assets/css/global.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/axios.js'],
